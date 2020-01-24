450287c02cf4c68ca8ad1650c1a48a553828fdec
alex <ajt33@uw.edu>
2020-01-24 13:34:29 -0800

    Week 3 exercises

--- chapter-08-exercises/exercise-1/exercise.R
+++ chapter-08-exercises/exercise-1/exercise.R
@@ -1,28 +1,34 @@
-# Exercise 1: creating and accessing lists
+
+# Exercise 1 CH 8: creating and accessing lists
 
 # Create a vector `my_breakfast` of everything you ate for breakfast
-
+my_breakfast <- c("apples", "sandwich", "fries")
 
 # Create a vector `my_lunch` of everything you ate (or will eat) for lunch
-
+my_lunch <- c("sandwich", "apple")
 
 # Create a list `meals` that has contains your breakfast and lunch
-
-
+meals <- list(breakfast = my_breakfast, lunch = my_lunch)
+print(meals)
 # Add a "dinner" element to your `meals` list that has what you plan to eat 
 # for dinner
-
+meals$dinner <- c("steak", "7 up")
+print(meals)
 
 # Use dollar notation to extract your `dinner` element from your list
 # and save it in a vector called 'dinner'
+dinner <- meals$dinner
+print(dinner)
 
 
 # Use double-bracket notation to extract your `lunch` element from your list
 # and save it in your list as the element at index 5 (no reason beyond practice)
-
-
+meals[[5]] <- meals[["lunch"]]
+print(meals)
 # Use single-bracket notation to extract your breakfast and lunch from your list
 # and save them to a list called `early_meals`
+early_meals <- meals[c("breakfast", "lunch")]
+print(early_meals)
 
 
 
@@ -30,11 +36,31 @@
 
 # Create a list that has the number of items you ate for each meal
 # Hint: use the `lappy()` function to apply the `length()` function to each item
+length(meals$breakfast)
+length(meals$lunch)
+length(meals$dinner)
 
+num_items <- lapply(meals, length )
+print(num_items)
 
 # Write a function `add_pizza` that adds pizza to a given meal vector, and
 # returns the pizza-fied vector
+add_pizza <- function(meal_vector){
+  #add pizza to that vector
+  
+  new_vector <- c(meal_vector, "pizza")
+  
+  #return the new vector
+  return(new_vector)
+}
+
+add_pizza(c("nachos"))
+
 
 
 # Create a vector `better_meals` that is all your meals, but with pizza!
+lapply(meals, add_pizza)
+
+
+
 
